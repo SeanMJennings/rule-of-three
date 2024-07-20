0bdb9ae63aff1d7f3eb2ec5273aa0255a4fa58bc
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-07-20 13:40:28 +0100

    Move vue to client folder

---
+++ client/vue/src/state-machines/notes.states.ts
@@ -0,0 +1,24 @@
+﻿export const enum NotesListMachineStates {
+  empty = "empty",
+  readyToAddNotesLists = "readyToAddNotesLists",
+  addingNotesLists = "addingNotesLists",
+  assessingNotesList = "assessingNotesList",
+}
+
+export const enum NotesMachineStates {
+  empty = "empty",
+  addingNotes = "addingNotes",
+  choosingNotesToCarry = "choosingNotesToCarry",
+}
+
+export const NotesMachineCombinedStates = {
+  empty: NotesListMachineStates.empty,
+  readyToAddNotesLists: NotesListMachineStates.readyToAddNotesLists,
+  addingNotesListsEmpty: { addingNotesLists: NotesMachineStates.empty },
+  addingNotesListsAddingNotes: {
+    addingNotesLists: NotesMachineStates.addingNotes,
+  },
+  addingNotesListsChoosingNotesToCarry: {
+    addingNotesLists: NotesMachineStates.choosingNotesToCarry,
+  },
+};
---
+++ client/vue/src/types/types.ts
@@ -0,0 +1,10 @@
+﻿export type Id = { id: number };
+export type NotesList = Id & {
+  name: string;
+};
+export type Notes = Note[];
+export type Note = Id & {
+  content: string;
+  carried: boolean;
+  page: number;
+};
