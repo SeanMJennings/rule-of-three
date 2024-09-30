451f9ae63da79bb08bc57f70a90aa6b95b783ceb
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-09-30 10:00:09 +0100

    Fix azure func

--- server/src/app.py
+++ server/src/app.py
@@ -13,10 +13,15 @@ def create_app(notes_list_service):
     notes_list_handler_for_items = NotesListHandlerForItems.as_view(
         NotesListHandlerForItems.name(), notes_list_service
     )
+    __add_app_url(app, notes_list_handler_for_groups)
+    __add_app_url(app, notes_list_handler_for_items)
+    return app
+
+
+# routePrefix in host.json does not work, so this is used instead
+def __add_app_url(app, handler):
     app.add_url_rule(
-        NotesListHandlerForGroups.route(), view_func=notes_list_handler_for_groups
-    )
-    app.add_url_rule(
-        NotesListHandlerForItems.route(), view_func=notes_list_handler_for_items
+        "/api" + handler.route(),
+        view_func=handler,
     )
     return app
