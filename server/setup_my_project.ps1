0c02122ea6a3918a347f630f7314990cf887883b
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-10-02 16:41:57 +0100

    Migrate to uv

--- server/setup_my_project.ps1
+++ server/setup_my_project.ps1
@@ -1,39 +1,15 @@
 try {
-    choco --version
-    write-host "`Found Chocolatey: " -fore yellow
+    uv --version
+    write-host "`Found uv: " -fore yellow
+    uv venv
 }
 catch {
-    write-host "`Installing Chocolatey: " -fore yellow
-    Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-    if ($env:Path -split ';' -notcontains $env:ALLUSERSPROFILE + "\chocolatey\bin") {
-        [Environment]::SetEnvironmentVariable("Path", $env:Path + ";%ALLUSERSPROFILE%\chocolatey\bin", "User")
-    }
-}
- 
-try {
-    python --version
-    write-host "`Found Python: " -fore yellow
-}
-catch {
-    write-host "`Installing Python: " -fore yellow
-    choco install python --version=3.11.9
+    write-host "`Installing uv: " -fore yellow
+    winget install --id=astral-sh.uv  -e
     refreshenv
+    uv venv
 }
 
- 
-try {
-    poetry --version
-    write-host "`Found Poetry: " -fore yellow
-}
-catch {
-    write-host "`Installing Poetry: " -fore yellow
-    (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python
-    if ($env:Path -split ';' -notcontains $env:APPDATA + "\Python\Scripts") {
-        [Environment]::SetEnvironmentVariable("Path", $env:Path + ";%APPDATA%\Python\Scripts", "User")
-        powershell.exe -nologo
-    }
-}
-
-poetry install
+uv sync
 
 .\.venv\Scripts\Activate.ps1
