606e834d17445b0deb3dfe5f8cb5f9b825b0f0d5
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-11-09 19:01:46 +0000

    Styling

--- client/src/components/tasks/EditTasksListNameModal.vue
+++ client/src/components/tasks/EditTasksListNameModal.vue
@@ -39,7 +39,7 @@ const onSubmit = async () => {
     <div :class="editStyles.body">
       <div :class="editStyles.inputContainer">
         <input id="edit-task-list-name-input" v-model="the_name" :class="editStyles.input" type="text"/>
-        <ButtonIcon the_id="edit-task-list-name-submit" :icon="faSave" v-on:click="onSubmit"/>
+        <ButtonIcon the_id="edit-task-list-name-submit" :icon="faSave" v-on:click="onSubmit" :icon-style="editStyles.icon"/>
       </div>
       <span id="edit-task-list-name-character-count" :class="commonStyle.characterCount">{{the_name.length > 0 ? the_name.length + "/50" : "" }}</span>
     </div>
