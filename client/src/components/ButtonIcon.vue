435490ac46835b22cf8ba0594fe92f4aac5d8ce1
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-11-17 23:04:52 +0000

    Improve button icon border

--- client/src/components/ButtonIcon.vue
+++ client/src/components/ButtonIcon.vue
@@ -7,13 +7,12 @@ const props = defineProps<{
   the_id?: string
   disabled?: boolean
   icon: IconDefinition
-  iconStyle?: typeof styles
 }>();
 </script>
 
 <template>
   <button :id="the_id" :class="styles.button" :disabled="disabled">
-    <FontAwesomeIcon :id="`${the_id !== '' ? the_id + '-icon' : ''}`" :class="props.iconStyle" :icon="props.icon"/>
+    <FontAwesomeIcon :id="`${the_id !== '' ? the_id + '-icon' : ''}`" :icon="props.icon"/>
   </button>
 </template>
 
