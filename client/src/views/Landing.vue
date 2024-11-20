b9ef673a78d275ec98f29a27602c4f71340ad0c5
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-11-20 16:53:46 +0000

    align landing page steps

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -11,9 +11,11 @@ const auth0 = useAuth0();
       <h2>to doing</h2>
       <h2>or accepting</h2>
       <br/>
-      <span>1. Add up to 22 tasks to a list</span>
-      <span>2. Carry or remove tasks that were not ticked off</span>
-      <span>3. After two carries, tick or remove old tasks</span>
+      <div :class="style.instructionContainer">
+        <span>1. Add up to 22 tasks to a list</span>
+        <span>2. Carry or remove tasks that were not ticked off</span>
+        <span>3. After two carries, tick or remove old tasks</span>
+      </div>
       <div :class="style.linkContainer">
         <RouterLink id="tasks-link" :class="style.link" v-show="auth0.isAuthenticated.value" to="/tasks">Go to Tasks</RouterLink>
       </div>
