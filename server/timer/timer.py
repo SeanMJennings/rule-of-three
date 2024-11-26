8b280aeadcd31d2ed41989f903e99bdc0c5aa182
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-11-26 17:24:13 +0000

    Added keep alive timer for cold start

---
+++ server/timer/timer.py
@@ -0,0 +1,21 @@
+﻿from pathlib import Path
+
+import azure.functions as func
+import requests
+import urllib3
+import yaml
+
+urllib3.disable_warnings()
+path = Path(__file__).parent / "config.yaml"
+config = yaml.safe_load(open(path))
+
+the_url = config["url"]
+
+app = func.FunctionApp()
+
+
+@app.function_name(name="keep_alive_timer")
+@app.schedule(schedule="0 */5 * * * *", arg_name="keep_alive_timer", run_on_startup=False,
+              use_monitor=False)
+def keep_alive() -> None:
+    requests.get('https://' + the_url + '/api/keep-alive')
