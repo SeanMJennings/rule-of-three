7fa06fb5df16d05c9135030cea1dba830cff6370
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-11-27 18:56:59 +0000

    Remove keep-alive. Not needed

--- server/api/app.py
+++ server/api/app.py
@@ -1,5 +1,4 @@
-﻿from api.handlers.keep_alive_handlers import register_keep_alive_handlers
-from api.handlers.tasks_list_handlers import register_task_handlers
+﻿from api.handlers.tasks_list_handlers import register_task_handlers
 from flask import Flask
 from flask_cors import CORS
 from api.handlers.exception_handlers import handle_exception
@@ -9,6 +8,5 @@ def create_app(tasks_list_service):
     app = Flask(__name__)
     CORS(app)
     register_task_handlers(app, tasks_list_service)
-    register_keep_alive_handlers(app)
     app.errorhandler(Exception)(handle_exception)
     return app
--- server/tests/handlers/keep_alive_handlers_test.py
+++
@@ -1,8 +0,0 @@
-﻿from tests.handlers.keep_alive_handlers_steps import calling_keep_alive, the_keep_alive_is_called, an_app
-from tests.specification import *
-
-
-def test_can_call_keep_alive():
-    Given(an_app)
-    When(calling_keep_alive)
-    Then(the_keep_alive_is_called)
