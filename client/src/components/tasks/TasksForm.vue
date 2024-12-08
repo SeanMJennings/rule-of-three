648a95c8b96f4493d02ee930032fe30e241a073d
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-12-08 12:19:38 +0000

    Limit form width

--- client/src/components/tasks/TasksForm.vue
+++ client/src/components/tasks/TasksForm.vue
@@ -53,26 +53,28 @@ const remove = (id: string | number) => {
     <ButtonIcon :class="commonStyle.button" :icon="faCheck" title="" :disabled="true"/>
   </div>
   <TasksCounter :max-tasks=taskLimit :task-count="getTasks(snapshot.context).length"/>
-  <div :class="styles.container">
-    <div v-if="showAddTask(snapshot.value)" id="add-task" :class="styles.addTask">
-      <label :class="commonStyle.label" for="add-task-input">Add a Task</label>
-      <div :class="commonStyle.inputContainer">
-        <input id="add-task-input" v-model="model.taskText" :class="commonStyle.input" type="text"/>
-        <ButtonIcon title="Add task" :icon="faPlusSquare" :class="`${disabled() ? commonStyle.disabled : ''} ${commonStyle.icon}`" the_id="add-task-submit" v-on:click="submit()" :disabled="!_.isEqual(snapshot.value, TasksMachineCombinedStates.addingTasksListsAddingTasks)"/>
+  <div :class="styles.outerContainer">
+    <div :class="styles.container">
+      <div v-if="showAddTask(snapshot.value)" id="add-task" :class="styles.addTask">
+        <label :class="commonStyle.label" for="add-task-input">Add a Task</label>
+        <div :class="commonStyle.inputContainer">
+          <input id="add-task-input" v-model="model.taskText" :class="commonStyle.input" type="text"/>
+          <ButtonIcon title="Add task" :icon="faPlusSquare" :class="`${disabled() ? commonStyle.disabled : ''} ${commonStyle.icon}`" the_id="add-task-submit" v-on:click="submit()" :disabled="!_.isEqual(snapshot.value, TasksMachineCombinedStates.addingTasksListsAddingTasks)"/>
+        </div>
+        <span id="character-count" :class="commonStyle.characterCount">{{ model.taskText.length > 0 ? model.taskText.length + "/150" : "" }}</span>
       </div>
-      <span id="character-count" :class="commonStyle.characterCount">{{ model.taskText.length > 0 ? model.taskText.length + "/150" : "" }}</span>
     </div>
-  </div>
-  <div :class="styles.taskList">
-    <Task v-for="task in getTasks(snapshot.context)"
-          :task="task"
-          :key="task.id + '.' + task.page" 
-          :carry="carry"
-          :remove="remove"
-          :tick="tick"
-          :show-carry-action="carryingOrRemovingTasks(snapshot.value) && canCarryTask(task)"
-          :show-remove-action="carryingOrRemovingTasks(snapshot.value) && canRemoveTask(task)"
-          :show-tick-action="showTickTasks(snapshot.value) && !task.ticked && !task.carried && !task.removed"
-    />
+    <div :class="styles.taskList">
+      <Task v-for="task in getTasks(snapshot.context)"
+            :task="task"
+            :key="task.id + '.' + task.page"
+            :carry="carry"
+            :remove="remove"
+            :tick="tick"
+            :show-carry-action="carryingOrRemovingTasks(snapshot.value) && canCarryTask(task)"
+            :show-remove-action="carryingOrRemovingTasks(snapshot.value) && canRemoveTask(task)"
+            :show-tick-action="showTickTasks(snapshot.value) && !task.ticked && !task.carried && !task.removed"
+      />
+    </div>
   </div>
 </template>
