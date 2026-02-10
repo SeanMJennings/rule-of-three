7ceb78de06849b820d9c4d3b8e8af7fbdd272603
SeanJennings <seanjennings976@gmail.com>
2026-02-10 20:18:10 +0000

    fix

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,8 +1,11 @@
 import {fileURLToPath, URL} from 'node:url'
 import {configDefaults, defineConfig} from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import {loadEnv} from "vite";
 
 export default defineConfig({
+    plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
